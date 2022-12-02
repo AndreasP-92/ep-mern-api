b7c8f477ac8fd9f276554145fe3083abf72e5ff6
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2022-12-02 23:31:36 +0100

    done

--- app.ts
+++ app.ts
@@ -2,7 +2,6 @@ export { };
 
 const express = require('express');
 const connectMongo = require('./startup/mongoDB')
-const connectMysql = require("./startup/mysql.js");
 
 const cors = require('cors');
 const path = require('path')
@@ -21,21 +20,25 @@ app.use(express.json());
 
 require('./controller/routes')(app);
 
-// connectMongo();
+switch(process.env.CONNECTED_DB){
+  case 'mysql' :
+    const db = require('./startup/mysql.js')
 
+    db.sequelize.sync()
+    .then(() => {
+      console.log("Synced db.");
+    })
+    .catch((err: any) => {
+      console.log("Failed to sync db: " + err.message);
+    });
 
-
-connectMysql.connectMysql(connectMysql.initializeMysql());
-
-// const db = require('./startup/mysql')
-
-// db.sequelize.sync()
-// .then(() => {
-//   console.log("Synced db.");
-// })
-// .catch((err:any) => {
-//   console.log("Failed to sync db: " + err.message);
-// });
+    break;
+case 'mongo' :
+    connectMongo();
+    break;
+case 'neo4j' :
+    break;
+}
 
 const port = process.env.PORT || 8080;
 
--- startup/mysql.js
+++ startup/mysql.js
@@ -2,12 +2,7 @@ require('dotenv').config();
 const Sequelize = require("sequelize");
 
 
-
-
-// module.exports = db;
-
-const initializeMysql = () => {
-  const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASS, {
+const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASS, {
     host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     dialect: 'mysql',
@@ -19,32 +14,13 @@ const initializeMysql = () => {
       acquire: 30000,
       idle: 10000
     }
-  });
+});
 
-  const db = {};
+const db = {};
 
-  db.Sequelize = Sequelize;
-  db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
-  db.users = require("../Model/mysql/Users")(sequelize, Sequelize);
+db.users = require("../Model/mysql/Users")(sequelize, Sequelize);
 
-  return db;
-}
-
-const connectMysql = (db) =>{
-
-
-  db.sequelize.sync()
-  .then(() => {
-    console.log("Synced db.");
-  })
-  .catch((err) => {
-    console.log("Failed to sync db: " + err.message);
-  });
-}
-
-
-module.exports = {
-  initializeMysql,
-  connectMysql
-}
+module.exports = db;
