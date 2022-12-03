37f81c3037fadd63350f2a9ddd1ed3680f7e5173
abdulahi roble <abdulahi_2610@hotmail.com>
2022-12-03 14:21:03 +0100

    Create user

--- app.ts
+++ app.ts
@@ -13,8 +13,8 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./repository/swagger.json');
 
 app.use(cors());
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json({limit: '50mb'}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
@@ -22,27 +22,26 @@ app.use(express.json());
 
 require('./controller/routes')(app);
 
-connectNeo4j()
 
-// switch(process.env.CONNECTED_DB){
-//   case 'mysql' :
-//     const db = require('./startup/mysql.js')
+switch (process.env.CONNECTED_DB) {
+  case 'mysql':
+    const db = require('./startup/mysql.js')
 
-//     db.sequelize.sync()
-//     .then(() => {
-//       console.log("Synced db.");
-//     })
-//     .catch((err: any) => {
-//       console.log("Failed to sync db: " + err.message);
-//     });
+    db.sequelize.sync()
+      .then(() => {
+        console.log("Synced db.");
+      })
+      .catch((err: any) => {
+        console.log("Failed to sync db: " + err.message);
+      });
 
-//     break;
-// case 'mongo' :
-//     connectMongo();
-//     break;
-// case 'neo4j' :
-//     break;
-// }
+    break;
+  case 'mongo':
+    connectMongo();
+    break;
+  case 'neo4j':
+    break;
+}
 
 const port = process.env.PORT || 8080;
 
---
+++ repository/Neo4jCollection/UserCollection.js
@@ -0,0 +1,65 @@
+// const db = require('../../startup/neo4j')
+const bcrypt = require('bcrypt')
+// const session = db()
+
+const neo4j = require('neo4j-driver');
+
+const uri = `${process.env.NEO4J_CONNECTIONSTRING}`
+const user = `${process.env.NEO4J_USER}`;
+const password = `${process.env.NEO4J_PASSWORD}`
+
+// To learn more about the driver: https://neo4j.com/docs/javascript-manual/current/client-applications/#js-driver-driver-object
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+
+const session = driver.session();
+
+module.exports = {
+    createUser: async (body) => {
+        const salt = await bcrypt.genSalt(10);
+        body.password = await bcrypt.hash(body.password, salt);
+
+
+        const firstname = body.firstname
+        const lastname = body.lastname
+        const email = body.email
+        const address = body.address
+        const zipcode = body.postal
+        const phone = body.number
+        const password = body.password
+
+        try {
+            async function createUsers() {
+                const result = await session.run(
+                    'CREATE (a:User {firstname: $firstname, lastname: $lastname, email: $email, address: $address, zipcode; $zipcode, phone: $phone, password: $password}) RETURN a',
+                    {
+                        firstname: firstname,
+                        lastname: lastname,
+                        email: email,
+                        address: address,
+                        zipcode: zipcode,
+                        phone: phone,
+                        password: password
+                    }
+                )
+
+                const singleRecord = result.records[0]
+                const node = singleRecord.get(0)
+
+                return {
+                    success: true,
+                    object: createUsers(),
+                    msg: "",
+                    status: 200
+                }
+            }
+
+        } catch (error) {
+            console.error(`Something went wrong: ${error}`);
+        } finally {
+            // Close down the session if you're not using it anymore.
+            await session.close();
+        }
+        await driver.close()
+    }
+
+}
