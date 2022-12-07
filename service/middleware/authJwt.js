cedf3ea132a6a900e6214f3e6acd2ca1d36aeaa1
Jeffrey <jeffoech@gmail.com>
2022-12-07 22:13:17 +0100

    Made all ts files into js files

--- service/middleware/authJwt.js
+++ service/middleware/authJwt.js
@@ -1,8 +1,7 @@
-export { }
 const jwt = require('jsonwebtoken')
 require('dotenv').config();
 
-const verify = (req: any, res: any, next: any) => {
+const verify = (req, res, next) => {
     let token = req.headers["x-access-token"];
 
     if (!token) {
@@ -10,7 +9,7 @@ const verify = (req: any, res: any, next: any) => {
         return res.status(403).json({message: "No token provided", veryfied: false})
     }
 
-    jwt.verify(token, process.env.JWT_PRIVATE_KEY, (error: any, decoded: any) => {
+    jwt.verify(token, process.env.JWT_PRIVATE_KEY, (error, decoded) => {
         if (error) {
             return res.status(401).json({message: "Unauthorized " + error, veryfied: false})
         }
