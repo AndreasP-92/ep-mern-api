9a768efdcdb0066cf3fb9f8c569fcb5517e07853
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2022-12-12 20:46:44 +0100

    integration and unitt test

---
+++ test/Integration/User.test.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+require('dotenv').config();
+
+
+describe('Check if /api/allEvents returns 200', () => {
+    test('Get successful result of the API call', async () => {
+        const testUser = {
+            "email" : "m@m.dk",
+            "password" : "1234",
+            "key" : process.env.JWT_PRIVATE_KEY
+        }
+        await axios.post('http://localhost:8080/api/login', testUser)
+            .then(r => {
+                expect(r.status).toBe(202);
+            })
+            .catch(e => {
+                throw new Error;
+            });
+    });
+});
