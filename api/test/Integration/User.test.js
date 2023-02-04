9aef9260ed7d8c5de3d7c0c3d9e73d78077bf37d
Jeffrey <jeffoech@gmail.com>
2023-02-04 14:54:08 +0100

    test4

--- api/test/Integration/User.test.js
+++ api/test/Integration/User.test.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 describe('Check if /api/allEvents returns 200', () => {
     test('Get successful result of the API call', async () => {
         const testUser = {
-            "email" : "m@m.dk",
+            "email" : "a@a.dk",
             "password" : "1234",
             "key" : process.env.JWT_PRIVATE_KEY
         }
