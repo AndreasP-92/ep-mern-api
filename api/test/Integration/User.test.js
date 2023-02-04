72bb2e475693ce1bafefc634da74a9e1b39afe64
Jeffrey <jeffoech@gmail.com>
2023-02-04 14:51:17 +0100

    test 3

--- api/test/Integration/User.test.js
+++ api/test/Integration/User.test.js
@@ -1,26 +1,26 @@
-// const axios = require('axios');
-// require('dotenv').config();
+const axios = require('axios');
+require('dotenv').config();
 
 
-// describe('Check if /api/allEvents returns 200', () => {
-//     test('Get successful result of the API call', async () => {
-//         const testUser = {
-//             "email" : "m@m.dk",
-//             "password" : "1234",
-//             "key" : process.env.JWT_PRIVATE_KEY
-//         }
-//         await axios.post('http://207.154.228.42:3001/api/login', testUser)
-//             .then(r => {
-//                 expect(r.status).toBe(202);
-//             })
-//             .catch(e => {
-//                 throw new Error;
-//             });
-//     });
-// });
+describe('Check if /api/allEvents returns 200', () => {
+    test('Get successful result of the API call', async () => {
+        const testUser = {
+            "email" : "m@m.dk",
+            "password" : "1234",
+            "key" : process.env.JWT_PRIVATE_KEY
+        }
+        await axios.post('http://207.154.228.42:3001/api/login', testUser)
+            .then(r => {
+                expect(r.status).toBe(202);
+            })
+            .catch(e => {
+                throw new Error;
+            });
+    });
+});
 
-test('tmp test', () => {
+// test('tmp test', () => {
 
   
-    expect(5).toEqual(5);
-  });
+//     expect(5).toEqual(5);
+//   });
